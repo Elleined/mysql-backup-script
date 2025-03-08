a587763b77d97b230a1946bee14b3fe310afe455
elleined <demadegu@gmail.com>
2025-03-08 17:22:28 +0800

    Make .bat and .sh script more reabable by using variable and also update readme

--- sh/single-db-backup.sh
+++ sh/single-db-backup.sh
@@ -1,6 +1,23 @@
+#!/bin/bash
+
+# Define variables
 MYSQL_USERNAME=...
 MYSQL_PASSWORD=...
 DATABASE_NAME=...
 BACKUP_PATH=...
 
-/usr/bin/mysqldump -u $MYSQL_USERNAME -p$MYSQL_PASSWORD $DATABASE_NAME --routines --triggers --events > $BACKUP_PATH/$DATABASE_NAME_backup_$(date +%F.%H%M%S).sql
+# Ensure backup directory exists
+mkdir -p "$BACKUP_PATH"
+
+# Generate timestamped backup filename
+BACKUP_FILE="$BACKUP_PATH/${DATABASE_NAME}_backup_$(date +%F_%H-%M-%S).sql"
+
+# Perform the backup
+/usr/bin/mysqldump -u "$MYSQL_USERNAME" -p"$MYSQL_PASSWORD" --routines --triggers --events "$DATABASE_NAME" > "$BACKUP_FILE"
+
+# Check if the backup was successful
+if [ $? -eq 0 ]; then
+    echo "Backup successful: $BACKUP_FILE"
+else
+    echo "Backup failed!" >&2
+fi
